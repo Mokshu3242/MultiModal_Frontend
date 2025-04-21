ffcaa4b8a0fbef9bbfb6abdd7676202c303d3912
Mokshad Ketan Sankhe <72201878+Mokshu3242@users.noreply.github.com>
2025-04-22 02:59:32 +0530

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -37,7 +37,7 @@ const Register = () => {
 
     setIsSendingOtp(true);
     try {
-      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/v1/otp/request`, {
+      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/--/api/v1/otp/request`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -83,7 +83,7 @@ const Register = () => {
 
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/api/v1/user/auth/register`,
+        `${import.meta.env.VITE_API_BASE_URL}/--/api/v1/user/auth/register`,
         {
           method: "POST",
           headers: {
